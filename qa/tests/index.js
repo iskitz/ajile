0d0874e0c3270f1d0232d8eb489a4953dcaebe78
Michael Lee <git@iskitz.com>
2012-06-04 05:41:46 -0700

    Unit Tests: Fixed script importing race condition in test suite launcher.

--- qa/tests/index.js
+++ qa/tests/index.js
@@ -1,52 +1,58 @@
 /*
- About	: Launch script for ajile's Jasmine Tests.
- Author	: Michael Lee (iskitz.com)
- Created	: 2011.12.17 @ 10:15 PM PT
- Updated	: 2012.05.23 @ 07:14 AM PDT
+ About   : Launch script for ajile's Jasmine Tests.
+ Author  : Michael Lee (iskitz.com)
+ Created : 2011.12.17 @ 10:15 PM PT
+ Updated : 2012.06.04 @ 04:44 AM PDT
 */
 
 (function (Ignore, Listen, undefined) {
 
-Include ("jasmine", "../../lib/jasmine/");
-
-Listen  ("jasmine", function jasmineCoreLoaded (moduleName) {
-
-	Ignore  (moduleName, arguments.callee);
-	Include ("jasmine-html", "../../lib/jasmine/");
-	Include ("jasmine.HtmlReporter");
-
-	Listen ("jasmine.HtmlReporter", function jasmineHTMLLoaded (moduleName) {
-
-		Ignore  (moduleName, arguments.callee);
-		Include	("net.ajile.test.*", "./");	
-
-		Listen  ("net.ajile.test.Namespace", function initializeJasmine (moduleName) {
-		
-			Ignore (moduleName, arguments.callee);
-
-			var jasmineEnv = jasmine && jasmine.getEnv();
-			jasmineEnv.updateInterval = 1000;
-			
-			var htmlReporter = new jasmine.HtmlReporter();
-			jasmineEnv.addReporter (htmlReporter);
-			
-			jasmineEnv.specFilter = function jasmineSpecFilter (spec) {
-				return htmlReporter.specFilter (spec);
-			};
-			
-			try {
-				jasmineEnv.execute();
-			}
-			catch (ex) {
-				var currentWindowOnload = window.onload;
-
-				window.onload = function windowLoaded () {
-					currentWindowOnload && currentWindowOnload();
-					jasmineEnv.execute();
-				};
-			}
-		});//End:initializeJasmine()
-	});//End:jasmineHTMLLoaded()
-});//End:jasmineCoreLoaded()
+   Include ("jasmine", "../../lib/jasmine/");
+   Listen  ("jasmine", function jasmineCoreLoaded (moduleName) {
+   
+      Ignore (moduleName, arguments.callee);
+      Load   ("../../lib/jasmine/jasmine-html.js");
+   
+      setTimeout (function(){ Include ("jasmine.HtmlReporter"); }, 0);
+   
+      Listen ("jasmine.HtmlReporter", function jasmineHTMLLoaded (moduleName) {
+   
+         Ignore  (moduleName, arguments.callee);
+         Include  ("net.ajile.test.*", "./");  
+   
+         Listen  (function initializeJasmine (moduleName) {
+   
+            var ns = net.ajile.test;
+   
+            if (!(ns.Load && ns.Namespace)) {
+               return;
+            }
+   
+            Ignore (arguments.callee);
+   
+            var jasmineEnv = jasmine && jasmine.getEnv();
+            jasmineEnv.updateInterval = 1000;
+            
+            var htmlReporter = new jasmine.HtmlReporter();
+            jasmineEnv.addReporter (htmlReporter);
+            
+            jasmineEnv.specFilter = function jasmineSpecFilter (spec) {
+               return htmlReporter.specFilter (spec);
+            };
+            
+            try {
+               jasmineEnv.execute();
+            }
+            catch (ex) {
+               var currentWindowOnload = window.onload;
+   
+               window.onload = function windowLoaded () {
+                  currentWindowOnload && currentWindowOnload();
+                  jasmineEnv.execute();
+               };
+            }
+         });//End:initializeJasmine()
+      });//End:jasmineHTMLLoaded()
+   });//End:jasmineCoreLoaded()
 
 })(Ajile.RemoveImportListener, Ajile.AddImportListener);
