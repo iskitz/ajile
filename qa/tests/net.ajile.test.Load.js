edbc69c4509233a70f4e5392ac189ef42c88797a
Michael Lee <git@iskitz.com>
2012-06-04 05:39:26 -0700

    Unit Tests: Added a new "Inline" spec to the Load Works suite to test loading inline code snippets.

--- qa/tests/net.ajile.test.Load.js
+++ qa/tests/net.ajile.test.Load.js
@@ -2,7 +2,7 @@
  About   : ajile's Load Tests Package.
  Author  : Michael Lee (iskitz.com)
  Created : 2011.12.17 @ 11:34 PM PT
- Updated : 2012.05.26 @ 01:33 AM PDT
+ Updated : 2012.06.04 @ 04:44 AM PDT
  */
 
 Namespace ("net.ajile.test");
@@ -19,6 +19,21 @@ Namespace ("net.ajile.test");
    });
 
    describe ("ajile: Load: Works", function defineAjileLoadWorksTests () {
+      it ("Load: Inline script works", function testAjileLoadInlineWorks () {
+
+         runs (function tryLoadInline () {
+            Load (null, null, "net.ajile.test.Load.inline = true;");
+         });
+
+         waitsFor (function didLoadInlineWork () {
+            return !!net.ajile.test.Load.inline;
+         }, "Load: Inline script failed", 500);
+
+         runs (function completeLoadInlineWorksTest() {
+            expect (net.ajile.test.Load.inline).toBe (true);
+         });
+      });
+
       it ("Load: External script works", function testAjileLoadWorks () {
 
          var works = false;
