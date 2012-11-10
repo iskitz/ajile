c648f9bdf80c4d64eece0abc7985362199048d14
iskitz <git@iskitz.com>
2012-11-10 02:54:40 -0800

    Unit Tests: Added initial ImportAs functionality tests.

--- qa/tests/net.ajile.test.ImportAs.js
+++ qa/tests/net.ajile.test.ImportAs.js
@@ -1,20 +1,43 @@
 /*
  About   : ajile's ImportAs Tests Package.
  Author  : Michael Lee (iskitz.com)
- Created : 2011.12.17 @ 11:34 PM PT
- Updated : 2012.05.25 @ 11:51 PM PDT
+ Created : 2011.12.17 @ 23:34 PM PT
+ Updated : 2012.11.10 @ 02:42 AM PDT
  */
 
 Namespace ("net.ajile.test");       // Define ajile's core tests' namespace.
 
 (net.ajile.test.ImportAs = function defineAjileImportAsTests (global, undefined) {
 
-   describe ("ajile: ImportAs: Exists", function defineAjileImportAsExistsTests () {
-      it ("ImportAs: exists", function testAjileImportAsExists () {
+   describe ("ajile: ImportAs:", function defineAjileImportAsExistsTests () {
+      it ("Exists in the global scope.", function testAjileImportAsExists () {
          expect (ImportAs).not.toBeNull();
          expect (ImportAs).not.toBeUndefined();
          expect (global.ImportAs).toBeDefined();
          expect (global.ImportAs).toBe (ImportAs);
+      });
+
+      it ("Creates alias in global scope", function testAjileImportAsWorks () {
+         net.ajile.test.ImportAs.Conflict = {works: true};
+         ImportAs ("Alias", "net.ajile.test.ImportAs.Conflict");
+         expect (Alias).not.toBeNull();
+         expect (Alias).not.toBeUndefined();
+         expect (Alias.works).toBe (true);
+         expect (net.ajile.test.ImportAs.Alias).not.toBeDefined();
+         expect (net.ajile.test.ImportAs.Conflict.works).toBe (true);
+         Ajile.Unload ("Alias");
+         Ajile.Unload ("net.ajile.test.ImportAs.Conflict");
+      });
+
+      it ("Doesn't create alias if name already exists in global scope", function testAjileImportAsPreserves () {
+         net.ajile.test.ImportAs.Conflict = {works: true};
+         global.Alias = "Alias";
+         ImportAs ("Alias", "net.ajile.test.ImportAs.Conflict");
+         expect (Alias).not.toBe (net.ajile.test.ImportAs.Conflict);
+         expect (net.ajile.test.ImportAs.Alias).not.toBeDefined();
+         expect (net.ajile.test.ImportAs.Conflict.works).toBe (true);
+         Ajile.Unload ("Alias");
+         Ajile.Unload ("net.ajile.test.ImportAs.Conflict");
       });
    });
 
