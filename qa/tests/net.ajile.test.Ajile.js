9be2d49ba499561510baf603cd77ad178d55d429
iskitz <git@iskitz.com>
2013-01-27 02:13:39 -0800

    ajile 1.3.8 : Added compressed and source versions for ajile 1.3.8. : Updated default compressed and source files to ajile 1.3.8. : handleImported(): Notifies generic listeners on successful imports. : Removed all ajile 1.3.7 files. : Removed unused ajile.1.2.1.flat.js.
    Docs
    : Ajile-Overview: Updated to reference ajile 1.3.8.
    
    Tests
    : GetVersion() now expects 1.3.8.

--- qa/tests/net.ajile.test.Ajile.js
+++ qa/tests/net.ajile.test.Ajile.js
@@ -1,8 +1,8 @@
 /*
  About   : ajile's core Tests Package.
  Author  : Michael Lee [iskitz.com]
- Created : 2011.12.17 @ 22:30 PST
- Updated : 2012.11.17 @ 04:31 PST
+ Created : 2011.12.17 @ 22:30-08.00
+ Updated : 2013.01.27 @ 01:30-08.00
  */
 
 Namespace ("net.ajile.test");
@@ -166,7 +166,7 @@ Namespace ("net.ajile.test");
          expect (Ajile.GetVersion).toBeDefined();
       });
       it ("GetVersion: works", function testAjileGetVersionWorks () {
-         expect (Ajile.GetVersion()).toEqual ("1.3.7");
+         expect (Ajile.GetVersion()).toEqual ("1.3.8");
       });
    });
 
